18418eebd55abb6338180f6c6ee887075190c308
YaskoL <yanis.oulal@efrei.net>
2025-01-09 13:28:20 +0100

    Yanis

---
+++ menu.js
@@ -0,0 +1,11 @@
+let menuMobile = document.querySelector('#menuMobile');
+let nav = document.querySelector('nav');
+
+
+menuMobile.addEventListener('click', function()
+{
+
+    nav.classList.toggle('helloNav');
+});
+
+console.log(menuMobile);
